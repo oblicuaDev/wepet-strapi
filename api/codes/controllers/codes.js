6d9389501ce67706f67452c39c3667e3480003e6
jdda95 <j.delgado@orekaconsultores.com>
2021-11-05 11:01:07 -0500

    services pet rel

--- api/codes/controllers/codes.js
+++ api/codes/controllers/codes.js
@@ -1,8 +1,18 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  create: async (ctx) => {
+    if (Array.isArray(ctx.request.body)) {
+      return await Promise.all(
+        ctx.request.body.map(strapi.services.codes.create)
+      );
+    } else {
+      return strapi.services.codes.create(ctx.request.body);
+    }
+  },
+};
