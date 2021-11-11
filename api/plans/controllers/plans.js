d69afa437e31158c175e7d23c6bb63407c5bf0d2
jdda95 <j.delgado@orekaconsultores.com>
2021-11-11 10:30:37 -0500

    services pet rel

--- api/plans/controllers/plans.js
+++ api/plans/controllers/plans.js
@@ -1,8 +1,23 @@
-'use strict';
-
+"use strict";
+const axios = require("axios");
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  create: async (ctx) => {
+    let entity = await strapi.services.plans.create(ctx.request.body);
+    const { data } = await axios.get(
+      `https://wepet.co/plattform/s/test/?id=${entity.id}`
+    );
+    return ctx.send(
+      {
+        msg: "Plan Created",
+        plan: entity,
+        resServicePhp: data,
+      },
+      201
+    );
+  },
+};
